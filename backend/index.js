e5c1cf4f3b59096d4afcc5a534ced7b49b099ec0
Remi <aligastudioopas@gmail.com>
2019-10-10 18:38:49 +0200

    First route exemples.

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (request, response) => {
+  response.send(
+    "Bienvenue sur le WildTicket_Version2 by Marine SAUSSÉ & Rémi CASTIEN"
+  );
+});
+
+app.listen(port, err => {
+  if (err) {
+    throw new Error("Something bad happened...");
+  }
+  console.log(`Server is listening on ${port}`);
+});
