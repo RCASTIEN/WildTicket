44018b13f1d12ad7979e79db7a4813959b22accf
saussemarine <saussemarine@gmail.com>
2019-10-16 17:43:59 +0200

    Adding bootstrap and starting to work on NavBar, logo added.

--- frontend/src/Components/NavBar/NavBar.jsx
+++ frontend/src/Components/NavBar/NavBar.jsx
@@ -1,13 +1,39 @@
-import React, { Component, Fragment } from "react";
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink } from 'reactstrap';
 
-class NavBar extends Component {
-  render() {
-    return (
-      <Fragment>
-        <h1>I'm going to be a great NavBar !</h1>
-      </Fragment>
-    );
-  }
+const NavBar = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/"><img alt="Logo" src="https://i.postimg.cc/JzMM2v9K/logo192.png" class="logo" /></NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink href="/Components/">FAQ</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/Components">Nous contacter</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/Components">Connexion</NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
 }
 
 export default NavBar;
