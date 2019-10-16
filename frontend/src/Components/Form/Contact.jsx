7377de2e8a10aafc2ad2c2a9e4d52f92774c0970
saussemarine <saussemarine@gmail.com>
2019-10-16 18:35:17 +0200

    Adding a formular contact.

--- frontend/src/Components/Form/Contact.jsx
+++ frontend/src/Components/Form/Contact.jsx
@@ -1,13 +1,31 @@
-import React, { Component, Fragment } from "react";
+import React from "react";
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
-class Contact extends Component {
-  render() {
+
+const Contact = (props) => {
     return (
-      <Fragment>
-        <h1>I'm going to be a great contact formular !</h1>
-      </Fragment>
-    );
-  }
+      <Form>
+        <FormGroup>
+          <Label for="exampleEmail">Email</Label>
+          <Input type="email" name="email" id="exampleEmail" placeholder="entrez votre adresse mail" />
+        </FormGroup>
+        <FormGroup>
+        <Label for="exampleSelect">Sujet</Label>
+        <Input type="select" name="select" id="exampleSelect" ple>
+          <option>Reporter un problème</option>
+          <option>Signalement d'un bug</option>
+          <option>Suggestion d'amélioration</option>
+          <option>Signaler un problème</option>
+          <option>Autre</option>
+        </Input>
+      </FormGroup>
+        <FormGroup>
+        <Label for="exampleText">Votre message</Label>
+        <Input type="textarea" name="text" id="message" placeholder="entrez votre message"  />
+      </FormGroup>
+        <Button>Envoyer</Button>
+      </Form>
+    )
 }
 
 export default Contact;
