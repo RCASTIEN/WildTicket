64e058572f4eb67bc8429749b7bb0813100fdef2
saussemarine <saussemarine@gmail.com>
2019-10-16 17:00:22 +0200

    Deleting Test and rearranging files for HomePage.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Home from "./router/Home";
-import Test from "./router/Test";
+import Home from "./Components/HomePage/Home";
 import "./App.css";
 
 class App extends Component {
@@ -10,7 +9,6 @@ class App extends Component {
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route path="/test" component={Test} />
         </Switch>
       </BrowserRouter>
     );
