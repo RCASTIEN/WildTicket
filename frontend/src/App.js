fe4aa7fbfdd54b07781c08c323cb57ac6fcc8e2b
Remi <aligastudioopas@gmail.com>
2019-10-20 20:06:46 +0200

    Add SCSS in project.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,21 +6,21 @@ import Home from "./Components/HomePage/Home";
 import Contact from "./Components/Form/Contact";
 import Connexion from "./Components/Form/Connexion";
 import Inscription from "./Components/Form/Inscription";
-import "./App.css";
+import "./App.scss";
 
 function App({ location }) {
   return (
     <div className="App">
       <NavBar />
-          <section className="route-section">
-            <Switch location={location} key={location}>
-              <Route exact path="/" component={Home} />
-              <Route path="/FAQ" component={FAQ} />
-              <Route path="/Contact" component={Contact} />
-              <Route path="/Connexion" component={Connexion} />
-              <Route path="/Inscription" component={Inscription} />
-            </Switch>
-        </section>
+      <section className="route-section">
+        <Switch location={location} key={location}>
+          <Route exact path="/" component={Home} />
+          <Route path="/FAQ" component={FAQ} />
+          <Route path="/Contact" component={Contact} />
+          <Route path="/Connexion" component={Connexion} />
+          <Route path="/Inscription" component={Inscription} />
+        </Switch>
+      </section>
     </div>
   );
 }
